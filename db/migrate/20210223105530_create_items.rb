08b87e865e81b9992d997e104f344d1c0d330573
ponsiro <tatsuya_hashida@yahoo.co.jp>
2021-02-23 20:09:53 +0900

    itemsテーブルの作成

--- db/migrate/20210223105530_create_items.rb
+++ db/migrate/20210223105530_create_items.rb
@@ -1,6 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
+      t.string :name,                    null: false
+      t.text :about,                     null: false
+      t.integer :category_id,            null: false
+      t.integer :status_id,              null: false
+      t.integer :delivery_fee_burden_id, null: false
+      t.integer :shipment_source_id,     null: false
+      t.integer :delivery_day_id,        null: false
+      t.integer :price,                  null: false
+      t.references :user,                null: false, foreign_key: true
 
       t.timestamps
     end
