7ab9cd454497a1854b5d7645e5fc5c534fc87864
ponsiro <tatsuya_hashida@yahoo.co.jp>
2021-02-23 13:32:14 +0900

    パスワードに英数字を含むバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i}
   validates :nickname, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
