1deb9ecfdcf442dd27b8ed4f5c9f8450bd4eaa7a
ponsiro <tatsuya_hashida@yahoo.co.jp>
2021-02-24 14:13:13 +0900

    ログインユーザーのみ出品ページへ遷移できる記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index]
   
   
   def index
   end
 
   def new
+    
   end
 
   def create
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :about, :category_id, :ststus_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
