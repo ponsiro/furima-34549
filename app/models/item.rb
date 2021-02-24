f608ef5512af00edfba6fe531f1f4f1037d3729c
ponsiro <tatsuya_hashida@yahoo.co.jp>
2021-02-24 16:27:01 +0900

    出品ページのプルダウンメニューの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,20 @@
 class Item < ApplicationRecord
 
-  
+  with_options presence: true do
+    validates :name
+    validates :about
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :ststus_id
+      validates :delivery_fee_burden_id
+      validates :shipment_source_id
+      validates :delivery_day_id
+    end
+    validates :price
+    validates :user, foreign_key: true
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :stutas
 end
