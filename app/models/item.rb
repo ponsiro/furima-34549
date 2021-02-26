151a30567f3014916f9d30ecc45dfc43858c50fc
ponsiro <tatsuya_hashida@yahoo.co.jp>
2021-02-26 12:20:55 +0900

    rubocopの実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,8 @@ class Item < ApplicationRecord
       validates :shipment_source_id
       validates :delivery_day_id
     end
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                      format: { with: /\A[0-9]+\z/ }
   end
 
   belongs_to :user
