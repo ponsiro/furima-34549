20d45315ee82e17f7d1e6b566e89a60d56efb5d2
ponsiro <tatsuya_hashida@yahoo.co.jp>
2021-03-01 15:10:33 +0900

    pathの変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,7 +47,7 @@ class ItemsController < ApplicationController
 
   def user_restrictions
     if current_user.id != @item.user_id
-      redirect_to new_user_session_path
+      redirect_to root_path
     end
   end
 end
