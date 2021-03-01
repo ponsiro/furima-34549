ca8941df6c9f5a0e1c9b3cae60ff0f565c3970d1
ponsiro <tatsuya_hashida@yahoo.co.jp>
2021-03-01 15:01:20 +0900

    修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :user_restrictions, only: [:edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -19,20 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    if user_signed_in?
-      redirect_to action: :index unless current_user.id == @item.user_id
-    else
-      redirect_to new_user_session_path
-    end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -45,5 +39,15 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :about, :category_id, :status_id, :delivery_fee_burden_id, :shipment_source_id,
                                  :delivery_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def user_restrictions
+    if current_user.id != @item.user_id
+      redirect_to new_user_session_path
+    end
   end
 end
