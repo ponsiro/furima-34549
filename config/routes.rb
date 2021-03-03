20d6e41cb4fabf27da2fb3a57f6324111dee2bb0
ponsiro <tatsuya_hashida@yahoo.co.jp>
2021-03-03 09:44:50 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items do
-    resources :purchase_histories, only: [:index]
+    resources :purchase_histories, only: [:index, :create]
   end
 
 end
