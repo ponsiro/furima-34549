c772ca657c554d6cad288cb2d31fbff9a2d928d3
ponsiro <tatsuya_hashida@yahoo.co.jp>
2021-03-04 14:49:40 +0900

    itemとuserのFactoryBotの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 4) }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { "test0000TEST" }
     password_confirmation { password }
     first_name            { "鈴木" }
     last_name             { "太郎" }
