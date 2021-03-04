3191c22c10c093b15cff9461d311bf5fcf288f74
ponsiro <tatsuya_hashida@yahoo.co.jp>
2021-03-04 11:54:47 +0900

    userのfactorybotの編集

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,11 +4,10 @@ FactoryBot.define do
     email                 { Faker::Internet.free_email }
     password              { Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
-    gimei = Gimei.name
-    first_name            { gimei.first.kanji }
-    last_name             { gimei.last.kanji }
-    first_name_kana       { gimei.first.katakana }
-    last_name_kana        { gimei.last.katakana }
+    first_name            { "鈴木" }
+    last_name             { "太郎" }
+    first_name_kana       { "スズキ" }
+    last_name_kana        { "タロウ" }
     birthday              { Faker::Date.backward }
   end
 end
